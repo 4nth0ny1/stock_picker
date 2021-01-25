a9e46dbfb9f0d90c1e1e77be3fdb3d101d1573c1
4anth0ny1 <acatullo4@gmail.com>
2021-01-24 23:31:56 -0600

    jan 24 changes

--- lib/stock_picker/cli.rb
+++ lib/stock_picker/cli.rb
@@ -1,8 +1,54 @@
 ## CLI Controller
 
 class StockPicker::CLI
+
     def call 
-        puts "yes CLI Controller is working."
-        @stock = StockPicker::Stock.today
+        greeting
+        menu_items
+        menu_logic   
+        @show_stock = StockPicker::Stock.show_stock
+        @get_additional_stock_information = StockPicker::Stock.get_additional_stock_information  
     end 
+
+    def greeting 
+        puts " "
+        puts "Welcome to your new stock portfolio builder."
+        puts "This program gets current stock information from the markets and allows you to build a portfolio."
+        puts "It even allows you to check your investments and quickly see how much money you've made."
+        puts " "
+    end 
+
+    def menu_items
+        puts "MAIN MENU"
+        puts "1. Analyze a stock"
+        puts "2. Check your portfolio"
+        puts "Type menu to come back to the menu."
+        puts "Type exit to exit the program."
+        puts " "
+    end 
+
+    def menu_logic
+        user_menu_input = nil
+        while user_menu_input != "exit"
+            puts "Choose a number to analyze a stock or check your portfolio:"
+            user_menu_input = gets.strip.downcase
+
+            if user_menu_input = "1"
+                StockPicker::Stock.show_stock
+                StockPicker::Stock.get_additional_stock_information
+            elsif "2"
+                puts "Let's check that portfolio..."                    ## ERROR ====  when i select 2 it goes to analyze a stock instead. 
+            elsif "menu"
+                menu_items
+            elsif "exit"
+                goodbye
+            else 
+                puts "your choices are: type ... 1, 2, menu or exit"
+            end  
+        end
+    end 
+
+    def goodbye
+        puts "Looks like your done. Goodbye"
+    end
 end 
--- lib/stock_picker/stock.rb
+++ lib/stock_picker/stock.rb
@@ -1,6 +1,56 @@
 class StockPicker::Stock
 
-    def self.today
-        puts "The Stock class is connected to the CLI."
+    def self.show_stock
+        puts "ANALYZE A STOCK"
+        ## gets the stock symbol from the user 
+        puts "Enter the stock symbol"                                       ## ERROR ====  if not valid symbol then repeat questions
+        stock_symbol_input = gets.strip.downcase 
+        ## displays the stock information 
+        puts "Apple, Inc. (symbol: AAPL)"
+        puts "Price: $299 per share"
+        puts " "
+        puts "Would you like to see additional financial information about this stock?"
+        puts "Enter: yes or no"
     end 
+
+    def self.get_additional_stock_information
+        ## this method shows additional financial information, like P/E ratios and others.
+        add_stock_info = gets.strip.downcase
+        if add_stock_info == "yes"
+            ## displays information 
+            puts " "
+            puts "hi, i'm more information about the stock"
+            puts " "
+            puts "Would you like to add this stock to your portfolio (type: add) or goto the main menu (type: menu)?"
+            add_to_portfolio_or_goto_menu = gets.strip.downcase
+            if add_to_portfolio_or_goto_menu = "add"
+                purchase_stock
+            elsif add_to_portfolio_or_goto_menu = "menu"
+                menu_items
+            else
+                "from here you can type add or menu"
+            end 
+        else
+            puts "(Type: add) to purchase the stock to your portfolio or (type: menu) to go back to the main menu."
+            add_to_portfolio_or_goto_menu = gets.strip.downcase
+                if add_to_portfolio_or_goto_menu = "add"
+                    purchase_stock
+                elsif add_to_portfolio_or_goto_menu = "menu"
+                    menu_items
+                else
+                    "from here you can type add or menu"
+                end  
+            end
+    end 
+
+    def self.purchase_stock
+        ## after the show_stock method runs, the user will be presented with the ability to analyze another stock, add this stock to the portfolio, or go back to the main menu.
+        portfolio = []
+        portfolio.collect do |stock|
+            portfolio << stock
+        end 
+        portfolio
+    end 
+
+
 end 
